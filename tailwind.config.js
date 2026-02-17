68d722aebfd4f9f7ab7e21848e63155b9c97a3ea
joaquinrodes <j.rodes@edu.salford.ac.uk>
2026-02-17 20:43:25 +0000

    feat(transactions): add internal scroll with hidden scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbarHide from "tailwind-scrollbar-hide"
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: "var(--color-primary)",
+        primary: 'var(--color-primary)',
       },
-      
     },
   },
-  plugins: [],
-};
+  plugins: [
+    scrollbarHide,
+  ],
+}
